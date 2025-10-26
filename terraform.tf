aeec065b6ddd87b39ea57f48a5e82c0b314b22c5
Sanoj Kumar <146000099+sanoj1994@users.noreply.github.com>
2025-10-26 09:34:33 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -9,7 +9,7 @@ terraform {
 
 
 backend "s3" {
-   bucket = "terraform-lock-state-bucket-sanojtech"
+   bucket = "terraform-lock-state-bucket-sanojtech1"
    key = "eks/terraform.tfstate"
    region = "ap-south-1"
    use_lockfile = true 
